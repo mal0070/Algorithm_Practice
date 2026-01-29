10cb070c6e46662f825f352798a49f38005491ed
Minah Lee <mal0070@konkuk.ac.kr>
2026-01-29 12:23:53 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+static int N;
+static vector<int> D;
+
+int main(){
+    cin>>N;
+    D.resize(N+1);
+    for(int i=2;i<=N;i++){
+        D[i] = D[i-1]+1;
+        if(i%2==0){
+            D[i] = min(D[i],D[i/2]+1);
+        }
+        if(i%3==0){
+            D[i] = min(D[i],D[i/3]+1);
+        }
+    }
+    cout<<D[N];
+    
+    return 0;
+}
