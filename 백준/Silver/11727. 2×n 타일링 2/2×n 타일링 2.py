de73a85b546e3c3dcd7cb66f23613c19f2e50249
Minah Lee <mal0070@konkuk.ac.kr>
2026-01-29 21:04:54 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -0,0 +1,14 @@
+n = int(input())
+#구하는 것: 2*n 크기의 직사각형을 채우는 방법 수
+if n == 1:
+    print(1)
+elif n == 2:
+    print(3)
+else:
+    dp = [0]*(n+1)
+    dp[1] = 1
+    dp[2] = 3
+    for i in range(3,n+1):
+        dp[i]=dp[i-1]+2*dp[i-2]
+    
+    print(dp[n]% 10007)
