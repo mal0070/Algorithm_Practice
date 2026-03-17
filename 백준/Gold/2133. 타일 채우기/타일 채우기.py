79bcc28b638d502107e043a53c9f47ff3a24d46e
Minah Lee <mal0070@konkuk.ac.kr>
2026-03-17 10:47:28 +0900

    [Gold IV] Title: 타일 채우기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2133. 타일 채우기/타일 채우기.py
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.py
@@ -6,11 +6,8 @@ n  = int(sys.stdin.readline())
 dp = [0 for _ in range(31)]
 
 dp[2] = 3
-
-if n>=4:
-    dp[4] = 11
     
-for i in range(6,n+1,2):
+for i in range(4,n+1,2):
     dp[i] = dp[i-2]*3 + sum(dp[:i-2])*2 + 2
         
 print(dp[n])
