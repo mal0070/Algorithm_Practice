3cd887ad4b5fb135a94921605c8d8bdb5d875102
Minah Lee <mal0070@konkuk.ac.kr>
2026-03-18 12:10:46 +0900

    [Silver III] Title: 파도반 수열, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.py
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -1,15 +1,14 @@
 import sys
 
 t  = int(sys.stdin.readline())
-cases = []
+
 for _ in range(t):
-    cases.append(int(sys.stdin.readline()))
+    n = int(sys.stdin.readline())
 
-dp = [0]*101
-#dp[i]: i번째에서 시도에서, 가장 긴 변의 길이
-dp[1],dp[2],dp[3] = 1,1,1
+    dp = [0]*101
+    #dp[i]: i번째에서 시도에서, 가장 긴 변의 길이
+    dp[1],dp[2],dp[3] = 1,1,1
 
-for n in cases:
     for i in range(4,n+1):
         dp[i] = max(dp[i-3]+dp[i-2], dp[i-1])
     print(dp[n])
