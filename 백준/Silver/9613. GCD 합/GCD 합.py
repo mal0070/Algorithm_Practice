5e2f2d37c09d29eb3e42ed0825f92208f2d8be24
Minah Lee <mal0070@konkuk.ac.kr>
2026-03-24 11:18:18 +0900

    [Silver IV] Title: GCD 합, Time: 260 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9613. GCD 합/GCD 합.py
+++ 백준/Silver/9613. GCD 합/GCD 합.py
@@ -1,26 +1,16 @@
 import sys
 
-def gcd(a, b):
+def gcd(a,b):
     while b:
-        a, b = b, a % b
+        a, b = b, a%b
     return a
 
-t = int(sys.stdin.readline())
 
+t = int(sys.stdin.readline())
 for _ in range(t):
     total = 0
-    
-    # 🔥 빈 줄 방지
-    while True:
-        line = sys.stdin.readline().strip()
-        if line:
-            nums = list(map(int, line.split()))
-            break
-
-    n = nums[0]
-
-    for i in range(1, n):
-        for j in range(i+1, n+1):
-            total += gcd(nums[i], nums[j])
-
+    nums = list(map(int, sys.stdin.readline().split()))
+    for i in range(1,len(nums)): #n을 신뢰하지 않기(사용자 입력이기 때문에) -> 실제 리스트 길이 기반 접근으로 방어적 코딩
+        for j in range(i+1,len(nums)):
+            total+=gcd(nums[i], nums[j])
     print(total)
