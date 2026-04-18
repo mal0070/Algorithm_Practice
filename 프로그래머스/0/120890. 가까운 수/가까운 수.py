ab4c7690b286ea58dcd6095937c23672acfb69a9
Minah Lee <leemina0815@gmail.com>
2026-04-18 22:05:43 +0900

    [level 0] Title: 가까운 수, Time: 0.01 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.py
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.py
@@ -1,9 +1,11 @@
 def solution(array, n):
-    differ = []
-    array.sort()
-    for i in array:
-        differ.append(abs(i-n))
+    #리스트를 만들지않고 해결
+    answer = array[0]
+    min_diff = abs(n-answer)
+    for x in array[1:]:
+        diff = abs(n-x)
+        if diff < min_diff or (diff == min_diff and answer > x):
+            min_diff = diff
+            answer = x
     
-    answer = [array[differ.index(min(differ))]]
-    
-    return min(answer)
+    return answer
