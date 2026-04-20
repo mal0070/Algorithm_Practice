4e0a2353703f4b1dedc6a49e5f12d5a0abc7da83
Minah Lee <leemina0815@gmail.com>
2026-04-20 13:49:18 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.04 ms, Memory: 9.37 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
@@ -1,15 +1,9 @@
 def solution(my_string): #자연수들의합
-    answer = 0
-    
     #알파벳 제거
     for s in my_string:
         if s.isalpha():
-            my_string = my_string.replace(s,",") #알파벳 제거, 구분자 삽입
+            my_string = my_string.replace(s," ") #알파벳 제거, 공백 삽입
     
     #숫자 합 계산
-    a = my_string.split(',')
-    for x in a:
-        if x.isdigit():
-            answer += int(x)
-    
-    return answer
+    a = my_string.split() #공백 기준으로 나눔 -> 깔끔하게 나눠짐
+    return sum(list(map(int, a)))
