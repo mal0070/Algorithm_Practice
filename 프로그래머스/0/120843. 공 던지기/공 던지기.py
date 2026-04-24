11f385f45e65add02798041f81cb76ba5f10af7f
Minah Lee <leemina0815@gmail.com>
2026-04-24 09:47:35 +0900

    [level 0] Title: 공 던지기, Time: 0.00 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.py
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.py
@@ -1,11 +1,2 @@
 def solution(numbers, k):
-    count = 1
-    i = 0
-    
-    while count < k: #count == k 이면 빠져나옴
-        i+=2 #공던짐
-        if i > len(numbers)-1: #last index 넘어가면
-            i = i - len(numbers)
-        count +=1
-    return numbers[i]
-
+    return numbers[2*(k-1) % len(numbers)]
